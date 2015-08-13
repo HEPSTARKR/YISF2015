0bdbbe1dab199a39c898ece3a1f70fd2508d0033
HEPSTAR <admin@hepstar.kr>
2015-08-14 03:06:58 +0900

    YISF Crypto 100

---
+++ crypto100.py
@@ -0,0 +1,38 @@
+from fractions import gcd
+global ss
+ss = [99,82,79,115,83,32,47,32,76,117,110,65,32,61,32,107,101,89,32,70,111,114,32,122,73,80,102,105,73,69]
+st = ""
+print gcd(198, 297),
+print gcd(164, 246),
+print gcd(158, 237),
+print gcd(230, 345),
+print gcd(166, 249),
+print gcd(64, 96),
+print gcd(94, 141),
+print gcd(64, 96),
+print gcd(152, 228)
+print gcd(234, 351),
+print gcd(220, 330),
+print gcd(130, 195),
+print gcd(64, 96),
+print gcd(122, 183),
+print gcd(64, 96),
+print gcd(214, 321),
+print gcd(202, 303),
+print gcd(178, 267),
+print gcd(64, 96),
+print gcd(140, 210),
+print gcd(222, 333),
+print gcd(228, 342),
+print gcd(64, 96),
+print gcd(244, 366),
+print gcd(146, 219),
+print gcd(160, 240),
+print gcd(204, 306),
+print gcd(210, 315),
+print gcd(146, 219),
+print gcd(138, 207),
+print ss
+for i in range(len(ss)):
+    st += chr(ss[i])
+print st
